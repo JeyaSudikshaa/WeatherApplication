c49bc9d587e1327f0b1c71c5282aeb3fa3729123
JeyaSudikshaa <83311327+JeyaSudikshaa@users.noreply.github.com>
2023-07-25 20:28:00 +0530

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment={
+    production:false,
+    weatherApiBaseUrl: 'https://weather-api99.p.rapidapi.com/weather',
+    // weatherApiBaseUrl:'https://ai-weather-by-meteosource.p.rapidapi.com/find_places?text=fishermans%20wharf&language=en',
+    // weatherApiBaseUrl:'https://forecast9.p.rapidapi.com/',
+    XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue:'weather-api99.p.rapidapi.com',
+    // XRapidAPIHostHeaderValue:'ai-weather-by-meteosource.p.rapidapi.com',
+    // XRapidAPIHostHeaderValue:'forecast9.p.rapidapi.com',
+    XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue:'93561b1c7emsh95a63850141966bp1c1708jsnae140d567ba6'
+    // XRapidAPIKeyHeaderValue:'93561b1c7emsh95a63850141966bp1c1708jsnae140d567ba6'
+    // XRapidAPIKeyHeaderValue:'93561b1c7emsh95a63850141966bp1c1708jsnae140d567ba6'
+};
